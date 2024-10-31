23644b78ab62cd6d30a9ed980065d257bb0d1060
Leticia Soto <samantasoto@gmail.com>
2024-10-31 14:45:21 +0000

    el reverse no se necesita tampoco invierto posicin de valores en divs y excluyo la posicion 0

--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -9,16 +9,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = ( {seconds} ) => {
 	
 	seconds = seconds + 100000;
-	const digit = String(seconds) .split('') .reverse();
+	const digit = String(seconds) .split('') ;
 
 	return (
 		<div className="row text-center text-bg-dark p-3 m-2 grid gap-3 fw-bold">
 			<div className="col align-middle clock fs-1 fw-bold bg-dark bg-gradient ms-5 rounded-2 px-1 py-3 border border-4 border-top-0 border-bottom-0"><FontAwesomeIcon icon={faClock}/></div>
-			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[4]}</div>
-			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[3]}</div>
-			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[2]}</div>
 			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[1]}</div>
-			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient me-5 rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[0]}</div>
+			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[2]}</div>
+			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[3]}</div>
+			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[4]}</div>
+			<div className="col align-middle digit fs-1 fw-bold bg-dark bg-gradient me-5 rounded-2 px-0 py-3 border border-4 border-top-0 border-bottom-0">{digit[5]}</div>
 		</div>
 	);
 };
